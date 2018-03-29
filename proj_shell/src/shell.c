9fabe8fcca43423faee537bcc3ab7ecb4ddef34f
cse <aurua57@gmail.com>
2018-03-29 22:20:17 +0900

    Add some comments, removed unwanted files

--- proj_shell/src/shell.c
+++ proj_shell/src/shell.c
@@ -6,7 +6,6 @@
  * "cd" command, pipe line, input redirection and etc, are not supported.  
  * 
  * 
- * 
  * @author JoongHoon Kim
  * @since 2018-03-27
  * 
@@ -24,8 +23,16 @@
 #define FALSE 0
 #define WHITE_SPACE_DELIM " \t\r\n\f\v"
 #define CMD_DELIM ";"
-#define BUFFER_SZ 65536
+#define BUFFER_SZ 65535
 #define MAX_CHILDS 4096
+/**
+ * Read single line of command input from "p_file" file descriptor, 
+ *  @param[in]      buf char buffer which is used for saving command. 
+ *  @param[in]      nbuf size of buffer -1
+ *  @param[in]      p_file file descriptor which connected to desired input stream.
+ *  @return         0 means something has been read
+ *                  -1 means empty string.
+ **/
 int getcmd(char *buf, int nbuf, FILE* p_file)
 {
     if(fgets(buf,nbuf,p_file))
@@ -34,9 +41,19 @@ int getcmd(char *buf, int nbuf, FILE* p_file)
         return -1;
 }
 
+/**
+ *  
+ *  Parse single command to execvp's arguments.
+ *  single command means it has no semicolon in that command.   
+ *  @warning        It calls strtok with cmd parameter.
+ *  @param[in]      cmd string of single command.
+ *  @return         execvp's arguments or NULL pointer.
+ **/
 char** parsecmdtoargs(char *cmd)
 {
+    //temp variable to save argv.
     char* tempargv[_POSIX_ARG_MAX];
+    //return variable should located in heap memory. 
     char** new_argv;
     int argc = 0, i = 0;
     char* token = strtok(cmd, WHITE_SPACE_DELIM);
@@ -44,8 +61,10 @@ char** parsecmdtoargs(char *cmd)
         tempargv[argc++]=token;
         token = strtok(NULL, WHITE_SPACE_DELIM);
     }
+    //if cmd has only white spaces or empty, returns NULL.
     if(argc == 0)
         return NULL;
+    //save temp argv to return variable.
     new_argv = (char**)malloc( sizeof(char*) * (argc+1) );
     for (i =0; i< argc; ++i) {
         new_argv[i] = tempargv[i];
@@ -57,14 +76,24 @@ char** parsecmdtoargs(char *cmd)
 
 int main(int argc, char **argv)
 {
+    //shell's input file descriptor, can be file or stdin.
     FILE * p_file = stdin;
-    char buffer[BUFFER_SZ];
+    //buffer for inputs.
+    char buffer[BUFFER_SZ+1];
+    //saves forked process's pid.
     int child_pids[MAX_CHILDS];
+    //the number of child that forked from parent.
     int num_of_child = 0;
+    //determines shell to run as batch mode or interactive mode.
     int is_batch = FALSE;
+    //in the case of quit command, it will turn to true.
     int has_end_sequence = FALSE;
+    //i variable used for looping.
     int i;
+
+    //if arguments are more than 2, it would be batch mode.
     if(argc >= 2) {
+        //try to open file, if it fails then end with error message.
         p_file = fopen (argv[1] , "r");
         is_batch = TRUE;
         if (p_file == NULL) { 
@@ -74,30 +103,46 @@ int main(int argc, char **argv)
     }
 
     {
-        char* token;
+        //this pointer will indicate string of single command.
+        char* single_command;
         char** new_argv;
         do{
+            //passed_chars is the number of characters that already processed from buffer.  
             int passed_chars = 0;
+
             if(!is_batch) {
                 printf("prompt> ");
             }
-            if(getcmd(buffer,BUFFER_SZ,p_file) != 0)
+            //read input from p_file and save to buffer.
+            if(getcmd(buffer,BUFFER_SZ,p_file) != 0) {
+                //if it reaches the end of file or get crrl+D, finish the shell.
                 return 1;
-            if(is_batch)    
+            }
+            
+            if(is_batch) {    
                 printf("%s", buffer);
+            }
+
+            //to deal with @l1(find @l1 below), extra null character is needed.
             buffer[strlen(buffer)+1] = '\0';
-            token = strtok(buffer, CMD_DELIM);
-            while(token) {
-                char tokened_cmd[BUFFER_SZ];
-                passed_chars += strlen(token)+1;
-                strcpy(tokened_cmd,token);
-                new_argv = parsecmdtoargs(tokened_cmd);
-                if(new_argv == NULL)
+            //to get single command, strtok buffer with semicolon.
+            single_command = strtok(buffer, CMD_DELIM);
+            while(single_command) {
+                //the length of single command and null characters are processed by strtok.
+                passed_chars += strlen(single_command)+1;
+                //tokenized single command will parsed to argv.
+                new_argv = parsecmdtoargs(single_command);
+                if(new_argv == NULL) {
                     break;
+                }
+                //if new_argv[0] equals to quit, it should be ended. 
                 if(strcmp(new_argv[0],"quit") == 0) {
                     has_end_sequence = TRUE;    
                     break;
                 }
+                //fork the process
+                // if it is parent then save child's pid to child_pids.
+                // if it is child the process will be changed by execvp. 
                 int pid = fork();
                 switch(pid) {
                     case -1:
@@ -113,14 +158,18 @@ int main(int argc, char **argv)
                         child_pids[num_of_child++] = pid;
                         break;
                 }
-                token = strtok(buffer+passed_chars, CMD_DELIM);
+                //@l1
+                //strtok was called with new pointer(from parsecmdtoargs function call)
+                //To tokenize the rest of buffer, strtok should be called with starting point to be parsed.  
+                single_command = strtok(buffer+passed_chars, CMD_DELIM);
                 free(new_argv);
             }
-            
+            //wait for children process to be ended.
             for(i=0; i<num_of_child; i++) {
                 waitpid(child_pids[i], NULL,0);
             }
             num_of_child = 0;
+        //if quit was called then end.
         }while(!has_end_sequence);
 
     }
